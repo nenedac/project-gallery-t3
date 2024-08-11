9e24bdf4e23285fe9977cca6f645d67c6cb873e5
Deyner Cruz <nenedac98@gmail.com>
2024-08-11 06:08:56 -0600

    scaffold UI

--- README.md
+++ README.md
@@ -30,8 +30,8 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 
 ## TODO
 
-- [ ] Make it deploy (vercel)
-- [ ] Scaffold basic ui with mock data
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
 - [ ] Tidy up build process
 - [ ] Actually set up a database (vercel postgres)
 - [ ] Attach database to UI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
+  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
+  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
+  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
+  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
+  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
+
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
