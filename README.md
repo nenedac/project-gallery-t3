576325a7132c8dc28b58678e7a8d129709537ec0
Deyner Cruz <nenedac98@gmail.com>
2024-08-12 00:47:56 -0600

    linking database to UI

--- README.md
+++ README.md
@@ -33,8 +33,8 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 - [x] Make it deploy (vercel)
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [ ] Actually set up a database (vercel postgres)
-- [ ] Attach database to UI
+- [x] Actually set up a database (vercel postgres)
+- [x] Attach database to UI
 - [ ] Add authentication (w/ clerk)
 - [ ] Add image upload
 - [ ] "taint" (server-only)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,18 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
-  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
-  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
-  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
-  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
-  "https://utfs.io/f/3dcd7533-b49e-4dc3-9012-ddbfb6de5d2e-3dcaim.jpg",
-
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="felx w-48 flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
