2a646aa5933d76b99e9c57888ed37e04415de5d8
Deyner Cruz <nenedac98@gmail.com>
2024-08-12 13:13:48 -0600

    replace: upload button for custom upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,29 +1,22 @@
-"use client";
-
+import { SimpleUploadButton } from "./simple-upload-button";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/dist/client/components/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+
 export function TopNav() {
- const router = useRouter();
 
- return (
-  <nav className="flex w-full items-center justify-between border-b 
+  return (
+    <nav className="flex w-full items-center justify-between border-b 
     p-4 text-xl font-semibold ">
-   <div>Catllery</div>
+      <div>Catllery</div>
 
-   <div className="flex flew-row">
-    <SignedOut>
-     <SignInButton />
-    </SignedOut>
-    <SignedIn>
-     <UploadButton endpoint="imageUploader"
-      onClientUploadComplete={() => {
-       router.refresh();
-      }}
-     />
-     <UserButton />
-    </SignedIn>
-   </div>
-  </nav>
- );
+      <div className="flex flew-row gap-4 items-center">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <SimpleUploadButton />
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
 }
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,7 +4,7 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number; }) {
  const image = await getImage(props.id);
 
- const uploaderInfo = await clerkClient.users.getUser(image.userId);
+ const uploaderInfo = await clerkClient().users.getUser(image.userId);
  return (
   <div className="flex h-full w-full min-w-0">
    <div className="flex flex-shrink items-center justify-center">
