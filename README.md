c5211168cccba0bf42bab6caf8b251bd65ff852a
Deyner Cruz <nenedac98@gmail.com>
2024-08-12 09:14:19 -0600

    adds: sentry for error handling

--- README.md
+++ README.md
@@ -39,9 +39,10 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 - [x] Add image upload
 - [x] "taint" (server-only)
 - [x] Use Next/Image component
-- [ ] Error management (w/ Sentry)
+- [x] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
 - [ ] Update upload button to be less cringe
 - [ ] Analytics (posthog)
 - [ ] Delete button (w/ Server Actions)
 - [ ] Ratelimiting (upstash)
+
---
+++ src/app/global-error.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import * as Sentry from "@sentry/nextjs";
+import NextError from "next/error";
+import { title } from "process";
+import { useEffect } from "react";
+
+export default function GlobalError(props: { error: unknown & { digest?: string; }; }) {
+  useEffect(() => {
+    Sentry.captureException(props.error);
+  }, [props.error]);
+
+  return (
+    <html>
+      <body>
+        {/* `NextError` is the default Next.js error page component. Its type
+        definition requires a `statusCode` prop. However, since the App Router
+        does not expose status codes for errors, we simply pass 0 to render a
+        generic error message. */}
+        <NextError statusCode={500} title="Error" />
+      </body>
+    </html>
+  );
+}
