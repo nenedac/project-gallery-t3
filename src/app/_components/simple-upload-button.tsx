5f3e88a51c7f63a6bedc0b6f7128f420a6413145
Deyner Cruz <nenedac98@gmail.com>
2024-08-12 14:45:14 -0600

    adds: styling and loading spinner for uploading state

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -32,9 +32,35 @@ const useUploadThingInputProps = (...args: Input) => {
 
 function UploadSVG() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="size-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
     </svg>
+  );
+}
+
+function LoadingSpinnerSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <circle className="spinner_1KD7" cx="12" cy="3" r="0" />
+      <circle className="spinner_1KD7 spinner_MJg4" cx="16.50" cy="4.21" r="0" />
+      <circle className="spinner_1KD7 spinner_SeO7" cx="7.50" cy="4.21" r="0" />
+      <circle className="spinner_1KD7 spinner_sj9X" cx="19.79" cy="7.50" r="0" />
+      <circle className="spinner_1KD7 spinner_7he2" cx="4.21" cy="7.50" r="0" />
+      <circle className="spinner_1KD7 spinner_WwCl" cx="21.00" cy="12.00" r="0" />
+      <circle className="spinner_1KD7 spinner_4zOl" cx="3.00" cy="12.00" r="0" />
+      <circle className="spinner_1KD7 spinner_vy2J" cx="19.79" cy="16.50" r="0" />
+      <circle className="spinner_1KD7 spinner_kugV" cx="4.21" cy="16.50" r="0" />
+      <circle className="spinner_1KD7 spinner_os1F" cx="16.50" cy="19.79" r="0" />
+      <circle className="spinner_1KD7 spinner_WNEg" cx="7.50" cy="19.79" r="0" />
+      <circle className="spinner_1KD7 spinner_l1Tw" cx="12" cy="21" r="0" />
+    </svg>
+
   );
 }
 
@@ -43,7 +69,10 @@ export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
-      toast("Uploading...", {
+      toast(
+        <div className="flex gap-2 text-white items-center">
+          <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
+        </div>, {
         duration: 100000,
         id: "upload-begin"
       });
@@ -65,7 +94,7 @@ export function SimpleUploadButton() {
         id="upload-button"
         type="file"
         className="sr-only"
-        {...inputProps}
+        { ...inputProps }
       />
     </div>
   );
